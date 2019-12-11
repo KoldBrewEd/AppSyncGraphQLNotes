c4f0a3516c3225b4f236df238096d744662167c0
Ed Lima <edlima@amazon.com>
2019-12-11 12:24:31 -0800

    Initial Datastore Implementation

--- src/App.js
+++ src/App.js
@@ -1,185 +1,199 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Amplify, {API,graphqlOperation} from 'aws-amplify';
-import { withAuthenticator} from 'aws-amplify-react'; 
-import aws_exports from './aws-exports'; // specify the location of aws-exports.js file on your project
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Amplify from "@aws-amplify/core";
+import { DataStore, Predicates } from "@aws-amplify/datastore";
+import { Note } from "./models";
+import { withAuthenticator } from "aws-amplify-react";
+import aws_exports from "./aws-exports"; // specify the location of aws-exports.js file on your project
 Amplify.configure(aws_exports);
 
-const createNote = `mutation createNote($note: String!){
-  createNote(input:{
-    note: $note
-  }){
-    __typename
-    id
-    note
-  }
-}`;
-
-const readNote = `query listNotes{
-  listNotes{
-    items{
-      __typename
-      id
-      note
-    }
-  }
-}`;
-
-const updateNote = `mutation updateNote($id: ID!,$note: String){
-  updateNote(input:{
-    id: $id
-    note: $note
-  }){
-    __typename
-    id
-    note
-  }
-}`;
-
-const deleteNote = `mutation deleteNote($id: ID!){
-  deleteNote(input:{
-    id: $id
-  }){
-    __typename
-    id
-    note
-  }
-}`;
-
-const searchNote = `query searchNotes($search: String){
-  searchNotes(filter:{note:{match:$search}}){
-    items{
-      id
-      note
-    }
-  }
-}`
-
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.state={
-      id:"",
-      notes:[],
-      searchResults:[],
-      value:"",
-      displayAdd:true,
-      displayUpdate:false,
-      displaySearch:false
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleUpdate = this.handleUpdate.bind(this);
-    this.handleSearch = this.handleSearch.bind(this);
-  }
-
-  async componentDidMount(){
-    const notes = await API.graphql(graphqlOperation(readNote));
-    this.setState({notes:notes.data.listNotes.items});
-  }
-
-  handleChange(event) {
-    this.setState({value:event.target.value});
-  }
-  async handleSubmit(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    const note = {"note":this.state.value}
-    await API.graphql(graphqlOperation(createNote, note));
-    this.listNotes();
-    this.setState({value:""});
-  }
-  async handleDelete(id) {
-    const noteId = {"id":id};
-    await API.graphql(graphqlOperation(deleteNote, noteId));
-    this.listNotes();
-  }
-  async handleUpdate(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    const note = {"id":this.state.id,"note":this.state.value};
-    await API.graphql(graphqlOperation(updateNote, note));
-    this.listNotes();
-    this.setState({displayAdd:true,displayUpdate:false,value:""});
-  }
-  async handleSearch(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    const search = {"search":this.state.value};
-    const result = await API.graphql(graphqlOperation(searchNote, search));
-    this.setState({searchResults:result.data.searchNotes.items,notes:[],displaySearch:true,value:""});   
-    if(JSON.stringify(result.data.searchNotes.items) === '[]'){
-      this.setState({searchResults:[{note:"No Match: Clear the search to go back to your Notes"}]});
-    };
-  }
-  selectNote(note){
-    this.setState({id:note.id,value:note.note,displayAdd:false,displayUpdate:true});
-  }
-  async listNotes(){
-    const notes = await API.graphql(graphqlOperation(readNote));
-    this.setState({notes:notes.data.listNotes.items,searchResults:[],displaySearch:false});
-  }
-  
-  render() {
-    const data = [].concat(this.state.notes)
-      .map((item,i)=> 
-      <div className="alert alert-primary show" role="alert">
-        <span key={item.i} onClick={this.selectNote.bind(this, item)}>{item.note}</span>
-        <button key={item.i} type="button" className="close" data-dismiss="alert" aria-label="Close" onClick={this.handleDelete.bind(this, item.id)}>
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      )
-    const searchResults = [].concat(this.state.searchResults)
-      .map((item,i)=> 
-        <div className="alert alert-success show" role="alert">
-          <span key={item.i}>{item.note}</span>
-        </div>
-      )
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Notes App</h1>
-        </header>
-        <br/>
-        <div className="container">
-          {this.state.displayAdd ?
-            <form onSubmit={this.handleSubmit}>
-              <div className="input-group mb-3">
-                <input type="text" className="form-control form-control-lg" placeholder="New Note" aria-label="Note" aria-describedby="basic-addon2" value={this.state.value} onChange={this.handleChange}/>
-                <div className="input-group-append">
-                  <button className="btn btn-primary border border-light" type="submit">Add Note</button>
-                  <button className="btn btn-primary border border-light" type="button" onClick={this.handleSearch}>Search</button>
-                </div>
-              </div>
-            </form>
-          : null }
-          {this.state.displayUpdate ?
-            <form onSubmit={this.handleUpdate}>
-              <div className="input-group mb-3">
-                <input type="text" className="form-control form-control-lg" placeholder="Update Note" aria-label="Note" aria-describedby="basic-addon2" value={this.state.value} onChange={this.handleChange}/>
-                <div className="input-group-append">
-                  <button className="btn btn-primary" type="submit">Update Note</button>
-                </div>
-              </div>
-            </form>
-          : null }
-        </div>
-        <br/>
-        <div className="container">
-          {searchResults}
-          {this.state.displaySearch ?
-            <button className="button btn-success float-right" onClick={this.listNotes.bind(this)}>
-              <span aria-hidden="true">Clear Search</span>
-            </button>
-          : null }
-          {data}
-        </div>
-      </div>
-    );
-  }
+async function listNotes(setNotes) {
+  const notes = await DataStore.query(Note, Predicates.ALL);
+  setNotes(notes);
 }
+
+function App() {
+  const [notes, setNotes] = useState([]);
+  const [value, setValue] = useState("");
+  const [id, setId] = useState("");
+  const [displayAdd, setDisplayAdd] = useState(true);
+  const [displayUpdate, setDisplayUpdate] = useState(false);
+  const [displaySearch, setDisplaySearch] = useState(false);
+
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    evt.stopPropagation();
+    DataStore.save(
+      new Note({
+        note: value
+      })
+    );
+    listNotes(setNotes);
+    setValue("");
+  };
+
+  async function handleSearch(evt) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    setDisplaySearch(true);
+    const search = await DataStore.query(Note, c => c.note("contains", value));
+    setNotes(search);
+    setValue("");
+  }
+
+  async function handleDelete(id) {
+    const toDelete = await DataStore.query(Note, id);
+    await DataStore.delete(toDelete);
+  }
+
+  async function handleSelect(note) {
+    setValue(note.note);
+    setId(note.id);
+    setDisplayUpdate(true);
+    setDisplayAdd(false);
+  }
+
+  async function handleUpdate(evt) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    const original = await DataStore.query(Note, id);
+    await DataStore.save(
+      Note.copyOf(original, updated => {
+        updated.note = value;
+      })
+    );
+    listNotes(setNotes);
+    setDisplayAdd(true);
+    setDisplayUpdate(false);
+    setValue("");
+  }
+
+  useEffect(() => {
+    listNotes(setNotes);
+    const subscription = DataStore.observe(Note).subscribe(msg => {
+      listNotes(setNotes);
+    });
+
+    const handleConnectionChange = () => {
+      const condition = navigator.onLine ? "online" : "offline";
+      console.log(condition);
+      if (condition === "online") {
+        listNotes(setNotes);
+      }
+    };
+
+    window.addEventListener("online", handleConnectionChange);
+    window.addEventListener("offline", handleConnectionChange);
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="jumbotron">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <div className="container">
+        {displayAdd ? (
+          <form>
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                className="form-control form-control-lg"
+                placeholder="New Note"
+                aria-label="Note"
+                aria-describedby="basic-addon2"
+                value={value}
+                onChange={e => setValue(e.target.value)}
+              />
+              <div className="input-group-append">
+                <button
+                  className="btn btn-warning border border-light"
+                  type="button"
+                  onClick={handleSubmit}
+                >
+                  Add Note
+                </button>
+                <button
+                  className="btn btn-warning border border-light"
+                  type="button"
+                  onClick={e => {
+                    handleSearch(e);
+                  }}
+                >
+                  Search
+                </button>
+              </div>
+            </div>
+          </form>
+        ) : null}
+        {displayUpdate ? (
+          <form
+            onSubmit={e => {
+              handleUpdate(e);
+            }}
+          >
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                className="form-control form-control-lg"
+                placeholder="Update Note"
+                aria-label="Note"
+                aria-describedby="basic-addon2"
+                value={value}
+                onChange={e => setValue(e.target.value)}
+              />
+              <div className="input-group-append">
+                <button className="btn btn-primary" type="submit">
+                  Update Note
+                </button>
+              </div>
+            </div>
+          </form>
+        ) : null}
+      </div>
+      <div className="container">
+        {notes.map((item, i) => {
+          return (
+            <div
+              className="alert alert-warning alert-dismissible show"
+              role="alert"
+            >
+              <span key={item.i} onClick={() => handleSelect(item)}>
+                {item.note}
+              </span>
+              <button
+                key={item.i}
+                type="button"
+                className="close"
+                data-dismiss="alert"
+                aria-label="Close"
+                onClick={() => {
+                  handleDelete(item.id);
+                  listNotes(setNotes);
+                }}
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+          );
+        })}
+        {displaySearch ? (
+          <button
+            className="button btn-warning float-right"
+            onClick={() => {
+              setDisplaySearch(false);
+              listNotes(setNotes);
+            }}
+          >
+            <span aria-hidden="true">Clear Search</span>
+          </button>
+        ) : null}
+      </div>
+    </div>
+  );
+}
+
 export default withAuthenticator(App, { includeGreetings: true });
