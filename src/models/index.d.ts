cd56dc99d6e996672ef786b0b66c94dbfb873fe5
Ed Lima <edlima@amazon.com>
2020-01-16 17:51:08 -0800

    Update models

--- src/models/index.d.ts
+++ src/models/index.d.ts
@@ -5,6 +5,7 @@ import { ModelInit, MutableModel, PersistentModelConstructor } from "@aws-amplif
 export declare class Note {
   readonly id: string;
   readonly note?: string;
+  readonly owner?: string;
   constructor(init: ModelInit<Note>);
   static copyOf(source: Note, mutator: (draft: MutableModel<Note>) => MutableModel<Note> | void): Note;
 }
